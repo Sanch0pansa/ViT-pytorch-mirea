8461fa1c6624d4ed9bb570c27e6180b30cdf1d09
Лебедев Александр <god@lifegame.su>
2023-11-21 18:37:59 +0300

    feat: add ModelCheckpoint callback, change log frequency

--- source/train.py
+++ source/train.py
@@ -2,25 +2,24 @@ import torch
 import lightning as L
 from source.data_module.BloodCellsDataModule import BloodCellsDataModule
 from source.model.ViT import ViT
+from lightning.pytorch.callbacks import ModelCheckpoint
 
-
-
-wandb_logger = L.pytorch.loggers.WandbLogger(project="ViT", log_model="all")
+wandb_logger = L.pytorch.loggers.WandbLogger(project="ViT")
 
 dm = BloodCellsDataModule()
 model = ViT(num_classes=dm.num_classes)
-# checkpoint_callback = ModelCheckpoint(dirpath='model-chkp/')
+checkpoint_callback = ModelCheckpoint(dirpath='model-chkp/')
 # early_stopping = EarlyStopping('val_loss')
 
 # log gradients, parameter histogram and model topology
-wandb_logger.watch(model, log="all")
+wandb_logger.watch(model, log_freq=1)
 # model.load_state_dict(torch.load(from_load))
 trainer = L.Trainer(
     max_epochs=5,
     accelerator="auto",
     devices=1,
     logger=wandb_logger,
-    # callbacks=[checkpoint_callback, early_stopping]
+    callbacks=[checkpoint_callback]
 )
 trainer.fit(model, dm)
-torch.save(model.state_dict(), "nn.pth")
+torch.save(model.state_dict(), "nets/nn1.pth")
