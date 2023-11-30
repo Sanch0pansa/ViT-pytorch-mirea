586db7c450d7059f782c71d96857e38cf1a88e0e
Лебедев Александр <god@lifegame.su>
2023-11-30 16:58:29 +0300

    feat: add hydra config to train process

--- source/train.py
+++ source/train.py
@@ -3,18 +3,20 @@ import lightning as L
 from source.data_module.BloodCellsDataModule import BloodCellsDataModule
 from source.model.ViT import ViT
 from lightning.pytorch.callbacks import ModelCheckpoint
+add import hydra
+from hydra.core.config_store import ConfigStore
+from source.conf.config import Params
 
 
+cs = ConfigStore.instance()
+cs.store(name="params", node=Params)
+
+
+@hydra.main(config_path="conf", config_name="config", version_base="1.3")
 def train(
-        depth: int = 4,
-        num_heads: int = 16,
-        embed_dim: int = 256,
+        cfg: Params,
         comment: str = "",
         filename_postfix: str = "",
-        epochs: int = 5,
-        learning_rate: float = 0.001,
-        dataset_name: str = "BloodCells+Augmentations",
-        drop_rate: float = 0.3,
         load_from_net: str = "",
         load_from_checkpoint: str = ""
 ):
@@ -34,33 +36,42 @@ def train(
     - load_from_net (str): Path to a pre-trained model to load weights from.
     - load_from_checkpoint (str): Path to a checkpoint to resume training from.
     """
-    architecture = f"Vit-D{depth}-H{num_heads}-E{embed_dim}"
-    run_name = f"Vit-D{depth}-H{num_heads}-E{embed_dim}{'-' + comment if comment != '' else ''}"
+    architecture = f"Vit-D{cfg.model.depth}-H{cfg.model.num_heads}-E{cfg.model.embed_dim}"
+    run_name = f"Vit-D{cfg.model.depth}-H{cfg.model.num_heads}-E{cfg.model.embed_dim}{'-' + comment if comment != '' else ''}"
     filename = f"../nets/{run_name}{'-' + filename_postfix if filename_postfix != '' else ''}.pth"
     wandb_logger = L.pytorch.loggers.WandbLogger(
         project="ViT",
         name=run_name,
         log_model="all",
         config={
-            "learning_rate": learning_rate,
+            "learning_rate": cfg.training.learning_rate,
             "architecture": architecture,
-            "dataset": dataset_name,
-            "epochs": epochs,
+            "dataset": cfg.data.name,
+            "epochs": cfg.training.epochs,
         }
     )
 
-    dm = BloodCellsDataModule()
+    dm = BloodCellsDataModule(
+        data_dir=cfg.data.path,
+        seed=cfg.data.seed,
+        batch_size=cfg.training.batch_size,
+        batch_size_cpu=cfg.training.batch_size_cpu,
+        augmentation_ratio=cfg.data.augmentation_ratio
+    )
     if load_from_checkpoint:
         model = ViT.load_from_checkpoint(f"./model-chkp/{load_from_checkpoint}")
 
     else:
         model = ViT(
-            num_classes=dm.num_classes,
-            depth=depth,
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            learning_rate=learning_rate,
-            drop_rate=drop_rate
+            num_classes=cfg.model.num_classes,
+            depth=cfg.model.depth,
+            embed_dim=cfg.model.embed_dim,
+            num_heads=cfg.model.num_heads,
+            learning_rate=cfg.training.learning_rate,
+            drop_rate=cfg.model.drop_rate,
+            optimizer=cfg.training.optimizer,
+            epochs=cfg.training.epochs,
+            data_loader_len=dm.len_of_train_dataset()
         )
 
         if load_from_net:
@@ -68,7 +79,7 @@ def train(
 
     checkpoint_callback = ModelCheckpoint(dirpath='model-chkp/')
     trainer = L.Trainer(
-        max_epochs=epochs,
+        max_epochs=cfg.training.epochs,
         accelerator="auto",
         devices=1,
         logger=wandb_logger,
@@ -81,4 +92,4 @@ def train(
 
 if __name__ == "__main__":
     # Example usage
-    train(depth=6, embed_dim=128, num_heads=8, comment="SGD", epochs=7, learning_rate=0.0001)
+    train()
